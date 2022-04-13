7d8a6f9751a1b5bfcb23615ee941411414122ee2
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2022-04-13 18:47:18 +0530

    uploading images  to Ipfs

--- src/components/AddNFT.jsx
+++ src/components/AddNFT.jsx
@@ -1,4 +1,7 @@
 import { Form, Button, Container } from "react-bootstrap";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
 function AddNFT() {
   return (
@@ -11,7 +14,28 @@ function AddNFT() {
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
-        
+
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>Default file input example</Form.Label>
+          <Form.Control
+            type="file"
+            onChange={async (e) => {
+              const file = e.target.files[0];
+              try {
+                const added = await client.add(file, {
+                  progress: (prog) => console.log(`received: ${prog}`),
+                });
+                console.log(added);
+                const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+                console.log(url);
+              } catch (error) {
+                console.log(error);
+              }
+              console.log("Hello MatherFucker King is Back");
+            }}
+          />
+        </Form.Group>
+
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
