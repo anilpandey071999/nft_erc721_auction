8fd3582283d15b682a43040f3b6af4f5d1d52f41
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2022-04-22 11:48:59 +0530

    Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,54 +4,111 @@ import {
   Button,
   Container,
   Alert,
+  Row,
   Card,
   ListGroup,
 } from "react-bootstrap";
 import MarketPlaceABI from "../abis/MarketPlaceAbi";
 import { ethers } from "ethers";
+import axios from "axios";
 
 const marketPlaceAddress = "0x07470D0B1080C3e11189cedC6041fA1396F33Bb3";
 
 function Home() {
- const [loadingState, setLoadingState] = useState("not-loaded");
-  const [nfts, setNfts] = useState([]); 
-   useEffect(()=>{
-    getAll()
-  })
+  const [account, setAccount] = useState("");
+  const [loadingState, setLoadingState] = useState("not-loaded");
+  const [nfts, setNfts] = useState([]);
+  useEffect(() => {
+    if (nfts <= 0) {
+      getAll();
+    }
+  });
   const getAll = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
+    const [acc] = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(acc);
+    console.log(
+      parseInt(account) ==
+        parseInt("0xb8bc57FF4C30e98eD5987B7A583ca34160934673")
+    );
     const MarketPlaceInstance = new ethers.Contract(
       marketPlaceAddress,
       MarketPlaceABI,
       signer
-      );
-    let a = await MarketPlaceInstance.getListedNft()
+    );
+    let marketContract = await MarketPlaceInstance.getListedNft();
+    const items = await Promise.all(
+      marketContract.map(async (i) => {
+        const meta = await axios.get(i.uri);
+        console.log(i[3]);
+        console.log("meta", meta.data);
+        let item = {
+          nftID: parseInt(i.nftID),
+          name: meta.data.name,
+          description: meta.data.description,
+          price: meta.data.price,
+          seller: i.seller,
+          uri: meta.data.uri,
+          openForSell: meta.data.openForSell,
+          sold: meta.data.sold,
+          openForAuction: meta.data.openForAuction,
+          stratAutionTiming: meta.data.stratAutionTiming,
+          endAutionTiming: meta.data.endAutionTiming,
+          autionBasePrice: meta.data.autionBasePrice,
+        };
+        console.log("Items->>", item);
+        return item;
+      })
+    );
+    console.log("Items", items);
+    setNfts(items);
   };
 
-  if(nfts.length <= 0) return(<Container class="fs-1 text-center"  style={{width: "64rem"}}>Market Place is Empty</Container>)
+  if (nfts.length <= 0)
+    return (
+      <Container class="fs-1 text-center" style={{ width: "64rem" }}>
+        Market Place is Empty
+      </Container>
+    );
   return (
     <div>
       <Container>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroupItem>Cras justo odio</ListGroupItem>
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-          </ListGroup>
-          <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
+        <Row md={4}>
+          {nfts.map((i, nft) => (
+            <Container>
+              <Card style={{ width: "18rem", padding: "0rem" }}>
+                <Card.Img
+                  variant="top"
+                  style={{ width: "18rem", height: "18rem" }}
+                  src={`${i.uri}`}
+                />
+                <Card.Body>
+                  <Card.Title>{i.name}</Card.Title>
+                  <Card.Text>{i.description}</Card.Text>
+                  <Card.Title>Price: {i.price} MDF</Card.Title>
+                  {parseInt(account) === parseInt(i.seller) ? (
+                    <Button
+                      style={{ width: "10rem", verticalAlign: "middle" }}
+                      onClick={() => "submitData()"}
+                    >
+                      Owner
+                    </Button>
+                  ) : (
+                    <Button
+                      style={{ width: "10rem", verticalAlign: "middle" }}
+                      onClick={() => "submitData()"}
+                    >
+                      BUY
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+            </Container>
+          ))}
+        </Row>
       </Container>
     </div>
   );
